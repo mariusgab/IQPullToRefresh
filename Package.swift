c722dca436a20e5726111e2f8a8a40039ca30d0f
mariusgab <gabrielmarius08@gmail.com>
2024-07-09 09:51:02 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "IQPullToRefresh",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v13)
     ],
     products: [
        .library(name: "IQPullToRefresh", targets: ["IQPullToRefresh"])
